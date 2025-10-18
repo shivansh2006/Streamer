2ea35baee8042df8ba6c698880d1649c1d5bb045
Shivansh <229474904+Shivansh-1702@users.noreply.github.com>
2025-10-18 23:42:59 +0530

    Update servers.ts

--- lib/providers/servers.ts
+++ lib/providers/servers.ts
@@ -1,10 +1,8 @@
 // lib/providers/servers.ts
 
-export const TEST_SERVERS = [
-  { name: "Server1", url: "https://server1.example.com" },
-  { name: "Server2", url: "https://server2.example.com" },
-];
-
-export function getServerList() {
-  return TEST_SERVERS;
+export function getAllServerStreams() {
+  return [
+    { name: "Server1", url: "https://server1.example.com" },
+    { name: "Server2", url: "https://server2.example.com" },
+  ];
 }
