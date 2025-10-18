c368cff88afca263a0324679e2d2696a7913a4e3
Shivansh <229474904+Shivansh-1702@users.noreply.github.com>
2025-10-18 23:46:12 +0530

    Migrate next.config.mjs to CommonJS format

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
+// next.config.js
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
-
-export default nextConfig;
