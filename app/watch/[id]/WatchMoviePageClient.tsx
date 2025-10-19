66e541116136bf0b740bce169bef01feb9bbdbdf
Shivansh <229474904+Shivansh-1702@users.noreply.github.com>
2025-10-19 10:43:52 +0530

    Update import path for PopupBlocker component

--- app/watch/[id]/WatchMoviePageClient.tsx
+++ app/watch/[id]/WatchMoviePageClient.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { PopupBlocker } from '@/components/PopupBlocker'
+import { PopupBlocker } from '../../../components/PopupBlocker'
 
 type Props = {
   movieId: string
