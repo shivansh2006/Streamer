2adf8980e98e32237c92d68a0d14fbdfb2b5e356
Shivansh <229474904+Shivansh-1702@users.noreply.github.com>
2025-10-19 10:51:57 +0530

    Update WatchTVPageClient.tsx

--- app/watch/tv/[id]/[season]/[episode]/WatchTVPageClient.tsx
+++ app/watch/tv/[id]/[season]/[episode]/WatchTVPageClient.tsx
@@ -3,7 +3,6 @@
 import { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { PopupBlocker } from '../../../../../../components/PopupBlocker'
 
 type Props = {
   tvId: string
