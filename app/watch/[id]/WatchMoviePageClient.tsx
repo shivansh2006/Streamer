8302fd6fbf0fd9264205aa6529e4e208e8204779
Shivansh singh <67850772+shivansh2006@users.noreply.github.com>
2025-10-20 20:48:59 +0530

    Update WatchMoviePageClient.tsx

--- app/watch/[id]/WatchMoviePageClient.tsx
+++ app/watch/[id]/WatchMoviePageClient.tsx
@@ -20,7 +20,6 @@ type Props = {
 }
 
 const EMBED_SERVERS = [
-  { name: '(Primary)(Regional) ', url: (id: string) => `https://player.smashy.stream/movie${id}` },
   { name: '(Primary1)(English/Regional) ', url: (id: string) => `https://moviesapi.club/movie/${id}` },
   { name: 'Server 1 ', url: (id: string) => `https://vidsrc.to/embed/movie/${id}` },
   { name: 'Server 2 ', url: (id: string) => `https://vidsrc.xyz/embed/movie/${id}` },
